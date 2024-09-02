161430731bc10110009ed1cbdd7b57cff38d9be1
LucasRossatto <154370022+LucasRossatto@users.noreply.github.com>
2024-09-02 16:42:51 -0300

    2.1

--- src/js/tv.js
+++ src/js/tv.js
@@ -1,18 +1,20 @@
 const API_Key = "c7a928a11e9e543b65e5d2f7b9f56e60";
 const Main_url = "https://api.themoviedb.org/3";
 const queryParams = new URLSearchParams(window.location.search);
-const id = queryParams.get('id');
+const id = queryParams.get("id");
 
 const fetchAndDisplayData = async () => {
   try {
-    const response = await fetch(`${Main_url}/tv/${id}?api_key=${API_Key}&language=pt-BR`);
+    const response = await fetch(
+      `${Main_url}/tv/${id}?api_key=${API_Key}&language=pt-BR`
+    );
     if (!response.ok) {
       throw new Error("Erro ao recuperar os dados da API");
     }
     const data = await response.json();
     const container = document.getElementById("container");
 
-    if (container) { 
+    if (container) {
       const card = createCard(data);
       container.appendChild(card);
     } else {
@@ -25,11 +27,10 @@ const fetchAndDisplayData = async () => {
 
 const createCard = (item) => {
   const card = document.createElement("article");
-  const backDropContainer = document.querySelector(".backGroundImg"); 
+  const backDropContainer = document.querySelector(".backGroundImg");
   card.className = "article-wrapper";
-  const BackdropUrl = `https://image.tmdb.org/t/p/w500${item.backdrop_path}`; 
+  const BackdropUrl = `https://image.tmdb.org/t/p/w500${item.backdrop_path}`;
   const imageUrl = `https://image.tmdb.org/t/p/w500${item.poster_path}`;
-
 
   card.innerHTML = `
     <div class="align-movie">
@@ -38,7 +39,9 @@ const createCard = (item) => {
       </div>
       <div class="project-info">
         <div class="flex-pr">
-          <div class="project-title text-nowrap">${item.title || item.name}</div>
+          <div class="project-title text-nowrap">${
+            item.title || item.name
+          }</div>
         </div>
         <div class="types">
           <span class="project-type">
@@ -48,12 +51,30 @@ const createCard = (item) => {
           <span class="project-type">
             ${item.release_date || item.first_air_date}
           </span>
-          <span class="project-type">${item.genres.map(genre => genre.name)}</span>
+          <span class="project-type">${item.genres.map(
+            (genre) => genre.name
+          )}</span>
         </div>
         <span class="project-tagline">"${item.tagline}"</span>
         <h1 class="project-desc-title" >Sinopse</h1>
         <span class="project-desc">${item.overview}</span>
+        
+            <span class="project-tagline">${
+              item.number_of_seasons
+            } Temporadas</span>
+
+      <span class="project-tagline">${item.number_of_episodes} Episodios</span>
+
+          
+
+      
+
+
       </div>
+
+
+
+
   `;
 
   if (backDropContainer) {
